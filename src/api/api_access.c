3c03d57798cd42a5925d2aae69fc9d31cbf08b14
Zou Shunxiang <shunxian@codeaurora.org>
2014-01-16 14:08:53 +0800

    [qca-ssdk] Remove user space related code from qca-ssdk
    qca-ssdk package is used to build out qca-ssdk.ko, so user
    space related code should be removed from it. Package qca-ssdk-shell
    is used to build ssdk_sh.
    Rewrite every files' license from proprietary into ISC license.
    Remove '\r' flag in each file.
    
    Change-Id: I421edc28625b61f55d4b92f81d07240432569fe2
    Signed-off-by: Zou Shunxiang <shunxian@codeaurora.org>

--- src/api/api_access.c
+++ src/api/api_access.c
@@ -1,11 +1,20 @@
 /*
  * Copyright (c) 2012 Qualcomm Atheros, Inc.
- * All Rights Reserved.
- * Qualcomm Atheros Confidential and Proprietary.
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include "sw.h"
 #include "fal.h"
+#if (defined(KERNEL_MODULE))
 #include "hsl.h"
 #include "hsl_dev.h"
 #if defined ATHENA
@@ -122,6 +131,7 @@
 #include "isisc_init.h"
 #include "isisc_reg_access.h"
 #include "isisc_reg.h"
+#endif
 #endif
 
 #include "sw_api.h"
