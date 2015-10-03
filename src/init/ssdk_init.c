4d66709a9ad9b61a05781493820e29ebe8654d79
linchen <linchen@codeaurora.org>
2015-10-03 10:03:35 +0800

    [qca-ssdk] support rmmod and insmod for ssdk
    qca-ssdk.ko could be rmmod and insmod again and again.
    No memory leak and crash.
    
    Change-Id: I551863c01d4ca78b5e9b63761900b8cdadc80c43
    Signed-off-by: linchen <linchen@codeaurora.org>

--- src/init/ssdk_init.c
+++ src/init/ssdk_init.c
@@ -1047,6 +1047,10 @@ qca_phy_mib_work_start(struct qca_phy_priv *priv)
 void
 qca_phy_mib_work_stop(struct qca_phy_priv *priv)
 {
+	if(!priv)
+		return;
+	if(priv->mib_counters)
+		kfree(priv->mib_counters);
 	cancel_delayed_work_sync(&priv->mib_dwork);
 }
 
@@ -1328,9 +1332,11 @@ qca_phy_remove(struct phy_device *pdev)
 {
 	struct qca_phy_priv *priv = pdev->priv;
 
-	if ((pdev->addr == 0) && priv) {
-        qca_phy_mib_work_stop(priv);
-		kfree(priv->mib_counters);
+	if ((pdev->addr == 0) && priv && (priv->sw_dev.name != NULL)) {
+		qca_phy_mib_work_stop(priv);
+#ifdef AUTO_SWITCH_RECOVERY
+		qm_err_check_work_stop(priv);
+#endif
 		unregister_switch(&priv->sw_dev);
 	}
 
@@ -1342,7 +1348,7 @@ qca_phy_remove(struct phy_device *pdev)
 static struct phy_driver qca_phy_driver = {
     .name		= "QCA AR8216 AR8236 AR8316 AR8327 AR8337",
 	.phy_id		= 0x004d0000,
-    .phy_id_mask= 0xffff0000,
+	.phy_id_mask= 0xffff0000,
 	.probe		= qca_phy_probe,
 	.remove		= qca_phy_remove,
 	.config_init= &qca_phy_config_init,
