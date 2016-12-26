83af3be801057bc68b0fc5d6bd9fe9c6bc32c581
linchen <linchen@codeaurora.org>
2016-12-26 18:04:08 +0800

    [qca-ssdk] Fix coverity issues
    this change is already on arugula release branch,
    I submit it on mainline also.
    
    Change-Id: I5af437997418d34513ff0902fd21ea4164839a2f
    Signed-off-by: linchen <linchen@codeaurora.org>

--- src/hsl/isis/isis_init.c
+++ src/hsl/isis/isis_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012, 2016, The Linux Foundation. All rights reserved.
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -144,7 +144,7 @@ isis_hw_init(a_uint32_t dev_id, ssdk_init_cfg *cfg)
 static sw_error_t
 isis_dev_init(a_uint32_t dev_id, hsl_init_mode cpu_mode)
 {
-    a_uint32_t entry;
+    a_uint32_t entry = 0;
     sw_error_t rv;
     hsl_dev_t *pdev = NULL;
 
