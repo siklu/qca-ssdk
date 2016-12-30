749c69b905cd6b3b7eaa3842314a51af776378e4
linchen <linchen@codeaurora.org>
2016-12-30 14:08:43 +0800

    [qca-ssdk] change ssdk version to 2.0.0
    Change-Id: I6d5d8769b58cf6e9802919ffa654e7fde34ca5ce
    Signed-off-by: linchen <linchen@codeaurora.org>

--- make/config.mk
+++ make/config.mk
@@ -93,7 +93,7 @@ endif
 BLD_DIR=$(PRJ_PATH)/build/$(OS)
 BIN_DIR=$(PRJ_PATH)/build/bin
 
-VER=1.3.0
+VER=2.0.0
 BUILD_NUMBER=$(shell cat $(PRJ_PATH)/make/.build_number)
 VERSION=$(VER).$(BUILD_NUMBER)
 BUILD_DATE=$(shell date -u  +%F-%T)
