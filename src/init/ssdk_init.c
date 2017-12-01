dc0a2870fa6f5a842f26738977ff6e2fe6b3fb55
xiaofeis <xiaofeis@codeaurora.org>
2017-12-01 17:44:39 +0800

    [qca-ssdk]: Need be probed on hawkeye
    Change-Id: I2d762ab654c1ab7dba0f914aec688f18516a4947
    Signed-off-by: xiaofeis <xiaofeis@codeaurora.org>

--- src/init/ssdk_init.c
+++ src/init/ssdk_init.c
@@ -1868,6 +1868,7 @@ static int ssdk_probe(struct platform_device *pdev)
 
 static const struct of_device_id ssdk_of_mtable[] = {
         {.compatible = "qcom,ess-switch" },
+	{.compatible = "qcom,ess-switch-ipq807x" },
         {}
 };
 
