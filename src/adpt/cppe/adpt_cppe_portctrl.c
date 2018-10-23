9f530a197df7bb9e894a97014bb98cd47c948489
esong <song@codeaurora.org>
2018-10-23 11:08:45 +0800

    [qca-ssdk] add cppe mtu and mru setting
    Change-Id: I6bcb6294cc87664381f22ce19029abb40cacbdb1
    Signed-off-by: esong <song@codeaurora.org>

--- src/adpt/cppe/adpt_cppe_portctrl.c
+++ src/adpt/cppe/adpt_cppe_portctrl.c
@@ -20,10 +20,13 @@
 #include "sw.h"
 #include "hppe_global_reg.h"
 #include "hppe_global.h"
+#include "hppe_portctrl_reg.h"
+#include "hppe_portctrl.h"
+#include "cppe_portctrl_reg.h"
+#include "cppe_portctrl.h"
 #include "adpt.h"
 #include "hsl.h"
 #include "hsl_dev.h"
-#include "ssdk_init.h"
 #include "ssdk_dts.h"
 #include "adpt_cppe_portctrl.h"
 
@@ -68,6 +71,97 @@ _adpt_cppe_port_mux_mac_set(a_uint32_t dev_id, fal_port_t port_id, a_uint32_t po
 	rv = cppe_port_mux_ctrl_set(dev_id, &cppe_port_mux_ctrl);
 
 	return rv;
+}
+
+sw_error_t
+adpt_cppe_port_mru_set(a_uint32_t dev_id, fal_port_t port_id,
+		fal_mru_ctrl_t *ctrl)
+{
+	sw_error_t rv = SW_OK;
+	union cppe_mru_mtu_ctrl_tbl_u mru_mtu_ctrl_tbl;
+
+	memset(&mru_mtu_ctrl_tbl, 0, sizeof(mru_mtu_ctrl_tbl));
+	ADPT_DEV_ID_CHECK(dev_id);
+	ADPT_NULL_POINT_CHECK(ctrl);
+
+	rv = cppe_mru_mtu_ctrl_tbl_get(dev_id, port_id, &mru_mtu_ctrl_tbl);
+	SW_RTN_ON_ERROR (rv);
+
+	mru_mtu_ctrl_tbl.bf.mru = ctrl->mru_size;
+	mru_mtu_ctrl_tbl.bf.mru_cmd = (a_uint32_t)ctrl->action;
+	rv = cppe_mru_mtu_ctrl_tbl_set(dev_id, port_id, &mru_mtu_ctrl_tbl);
+
+	return rv;
+}
+
+sw_error_t
+adpt_cppe_port_mru_get(a_uint32_t dev_id, fal_port_t port_id,
+		fal_mru_ctrl_t *ctrl)
+{
+	sw_error_t rv = SW_OK;
+	union cppe_mru_mtu_ctrl_tbl_u mru_mtu_ctrl_tbl;
+
+	memset(&mru_mtu_ctrl_tbl, 0, sizeof(mru_mtu_ctrl_tbl));
+	ADPT_DEV_ID_CHECK(dev_id);
+	ADPT_NULL_POINT_CHECK(ctrl);
+
+	rv = cppe_mru_mtu_ctrl_tbl_get(dev_id, port_id, &mru_mtu_ctrl_tbl);
+	SW_RTN_ON_ERROR (rv);
+
+	ctrl->mru_size = mru_mtu_ctrl_tbl.bf.mru;
+	ctrl->action = (fal_fwd_cmd_t)mru_mtu_ctrl_tbl.bf.mru_cmd;
+
+	return SW_OK;
+}
+
+sw_error_t
+adpt_cppe_port_mtu_set(a_uint32_t dev_id, fal_port_t port_id,
+		fal_mtu_ctrl_t *ctrl)
+{
+	sw_error_t rv = SW_OK;
+	union cppe_mru_mtu_ctrl_tbl_u mru_mtu_ctrl_tbl;
+
+	memset(&mru_mtu_ctrl_tbl, 0, sizeof(mru_mtu_ctrl_tbl));
+	ADPT_DEV_ID_CHECK(dev_id);
+	ADPT_NULL_POINT_CHECK(ctrl);
+
+	rv = cppe_mru_mtu_ctrl_tbl_get(dev_id, port_id, &mru_mtu_ctrl_tbl);
+	SW_RTN_ON_ERROR (rv);
+
+	mru_mtu_ctrl_tbl.bf.mtu = ctrl->mtu_size;
+	mru_mtu_ctrl_tbl.bf.mtu_cmd = (a_uint32_t)ctrl->action;
+	rv = cppe_mru_mtu_ctrl_tbl_set(dev_id, port_id, &mru_mtu_ctrl_tbl);
+	SW_RTN_ON_ERROR (rv);
+
+	if ((port_id >= SSDK_PHYSICAL_PORT0) && (port_id <= SSDK_PHYSICAL_PORT7))
+	{
+		rv = hppe_mc_mtu_ctrl_tbl_mtu_set(dev_id, port_id, ctrl->mtu_size);
+		SW_RTN_ON_ERROR (rv);
+		rv = hppe_mc_mtu_ctrl_tbl_mtu_cmd_set(dev_id, port_id, (a_uint32_t)ctrl->action);
+		SW_RTN_ON_ERROR (rv);
+	}
+
+	return rv;
+}
+
+sw_error_t
+adpt_cppe_port_mtu_get(a_uint32_t dev_id, fal_port_t port_id,
+		fal_mtu_ctrl_t *ctrl)
+{
+	sw_error_t rv = SW_OK;
+	union cppe_mru_mtu_ctrl_tbl_u mru_mtu_ctrl_tbl;
+
+	memset(&mru_mtu_ctrl_tbl, 0, sizeof(mru_mtu_ctrl_tbl));
+	ADPT_DEV_ID_CHECK(dev_id);
+	ADPT_NULL_POINT_CHECK(ctrl);
+
+	rv = cppe_mru_mtu_ctrl_tbl_get(dev_id, port_id, &mru_mtu_ctrl_tbl);
+	SW_RTN_ON_ERROR (rv);
+
+	ctrl->mtu_size = mru_mtu_ctrl_tbl.bf.mtu;
+	ctrl->action = (fal_fwd_cmd_t)mru_mtu_ctrl_tbl.bf.mtu_cmd;
+
+	return SW_OK;
 }
 
 /**
