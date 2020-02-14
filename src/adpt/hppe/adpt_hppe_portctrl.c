bec1966e74ea2daa6398c2c6092e5f80b113fe7b
zhongjia <zhongjia@codeaurora.org>
2020-02-14 14:49:11 +0800

    [qca-ssdk]:fix port interface mode apply issue.
    Change-Id: Ic876ee9ee1b7885d6b6f5166c2c3085a707d021a
    Signed-off-by: zhongjia <zhongjia@codeaurora.org>

--- src/adpt/hppe/adpt_hppe_portctrl.c
+++ src/adpt/hppe/adpt_hppe_portctrl.c
@@ -2956,6 +2956,7 @@ _adpt_hppe_port_phyaddr_update(a_uint32_t dev_id, a_uint32_t port_id,
 		port_phyinfo = ssdk_port_phyinfo_get(dev_id, port_id);
 		phy_addr = port_phyinfo->phy_addr;
 		qca_ssdk_phy_address_set(dev_id, port_id, phy_addr);
+		hsl_port_phy_access_type_set(dev_id, port_id, PHY_I2C_ACCESS);
 		SSDK_DEBUG("port %x phy_addr is %x\n", port_id, phy_addr);
 	}
 	else
@@ -2968,6 +2969,7 @@ _adpt_hppe_port_phyaddr_update(a_uint32_t dev_id, a_uint32_t port_id,
 			SW_RTN_ON_ERROR (rv);
 			phy_addr++;
 			qca_ssdk_phy_address_set(dev_id, port_id, phy_addr);
+			hsl_port_phy_access_type_set(dev_id, port_id, PHY_MDIO_ACCESS);
 			SSDK_DEBUG("port %x phy_addr is %x\n", port_id, phy_addr);
 		}
 	}
